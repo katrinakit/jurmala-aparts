025903a6755b5257dbd1e0103b5335181a982ce4
Your Name <you@example.com>
2024-02-28 10:10:07 +0200

    jurmala

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ with open('jurmala.csv', newline='', encoding='utf-8') as csv_file:
 apartments.pop(0)
 
 # print(apartments)
-
+print("apartments list command:")
+jurmala=apartments.copy()
+def sort_price(jurmala):
+    return int(jurmala[8])
 while True:
     print("1. Get apartments by sequence number")
     print("2. Top 10 by highest price")
@@ -28,25 +31,43 @@ while True:
     choice = input("Enter your choice (1-6): ")
 
     if choice == '1':
+        a=int(input("Please write apartaments sequence number: "))
+        print(jurmala[a-1])
         # https://www.w3schools.com/python/python_lists_access.asp
         pass
     elif choice == '2':
+        jurmala.sort(key = sort_price, reverse = True)
+        print(jurmala[0:10])
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '3':
+        jurmala.sort(key = sort_price)
+        print(jurmala[0:10])
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '4':
+        a=int(input("Please enter your highest price: "))
+        newlist = []
+        jurmala.sort(key = sort_price)
+        newlist=[x for x in jurmala if a>int(x[8])]
+        print(newlist[0:20])
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         # https://www.w3schools.com/python/python_lists_access.asp - Range of Indexes
         pass
     elif choice == '5':
+        a=int(input("Please enter your lowest price: "))
+        newlist = []
+        jurmala.sort(key = sort_price)
+        newlist=[x for x in jurmala if a<int(x[8])]
+        print(newlist[0:20])
+        
+
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         # https://www.w3schools.com/python/python_lists_access.asp - Range of Indexes
         pass
 
     elif choice == '6':
-        # 
+        print("some words of motivation.....buy, rent, get rich")
         pass
     elif choice == '7':
         print("Exiting")
